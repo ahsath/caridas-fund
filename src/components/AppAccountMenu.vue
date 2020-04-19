92e27442de21fb49677146efbed578eb558319bf
Ahsath <ahsath@gmail.com>
2020-04-19 00:36:19 -0500

    Add: AppAccountMenu.vue

---
+++ src/components/AppAccountMenu.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-menu offset-y>
+    <template #activator="{ on }">
+      <v-btn v-on="on" icon>
+        <v-avatar size="40">
+          <img :src="getPhotoURL" :alt="getName" />
+        </v-avatar>
+      </v-btn>
+    </template>
+    <v-list>
+      <v-list-item>
+        <v-list-item-title>
+          Cuenta
+          <v-chip label small>Pronto</v-chip>
+        </v-list-item-title>
+      </v-list-item>
+      <v-list-item @click="logout">
+        <v-list-item-title>Salir</v-list-item-title>
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
+
+<script>
+import { createNamespacedHelpers } from "vuex";
+const { mapGetters, mapActions } = createNamespacedHelpers("user");
+
+export default {
+  methods: {
+    ...mapActions(["logout"])
+  },
+  computed: {
+    ...mapGetters(["getPhotoURL", "getName"])
+  }
+};
+</script>
