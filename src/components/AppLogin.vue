2e456e5f780822f7b7e60644ccc4d3a9319a09c7
Ahsath <ahsath@gmail.com>
2020-04-19 00:37:07 -0500

    Update: AppLogin.vue

--- src/components/AppLogin.vue
+++ src/components/AppLogin.vue
@@ -1,15 +1,21 @@
 <template>
   <v-dialog v-model="open" max-width="320">
     <v-card elevation="24">
-      <v-card-title class="headline">Debes autenticarte para comenzar</v-card-title>
+      <v-card-title class="headline">{{ title }}</v-card-title>
       <v-card-text>
-        <v-btn class="normal-case" @click="signInWithPopup" light large block>
+        <v-btn class="normal-case" @click.stop="signin" light large block>
           <v-icon left>$vuetify.icons.values.google</v-icon>
           <div class="leading-none">Inicia sesión con Google</div>
         </v-btn>
       </v-card-text>
       <v-card-text>
-        <v-checkbox v-model="termsAndConditionsAccepted" hide-details="auto">
+        <v-checkbox
+          ref="checkbox"
+          v-model="accept"
+          hide-details="auto"
+          :error-messages="errorMessage"
+          :rules="[validateTermsAndConditionsRule]"
+        >
           <template #label>
             <div class="body-2">
               He leído y acepto los
@@ -27,12 +33,53 @@ import { createNamespacedHelpers } from "vuex";
 const { mapActions } = createNamespacedHelpers("user");
 
 export default {
+  props: {
+    title: {
+      type: String,
+      default: "Inicia sesión"
+    },
+    createRequestAfterLogin: {
+      type: Boolean,
+      default: false
+    }
+  },
   data: () => ({
     open: true,
-    termsAndConditionsAccepted: false
+    accept: false,
+    errorMessage: "",
+    errorMessageTemplate: "Debes aceptar los terminos y condiciones"
   }),
+  watch: {
+    accept(val) {
+      if (val) this.errorMessage = "";
+    }
+  },
   methods: {
-    ...mapActions(["signInWithPopup"])
+    ...mapActions(["signInWithPopup"]),
+    async signin() {
+      if (this.validateCheckbox()) {
+        try {
+          this.$emit("login:success", {
+            message: await this.signInWithPopup(),
+            createRequestAfterLogin: this.createRequestAfterLogin
+          });
+          this.open = false;
+        } catch (error) {
+          this.$emit("login:fail", error);
+          this.open = false;
+        }
+      }
+    },
+    validateCheckbox() {
+      if (!this.$refs.checkbox.validate()) {
+        this.errorMessage = this.errorMessageTemplate;
+        return;
+      }
+      return true;
+    },
+    validateTermsAndConditionsRule(val) {
+      return !!val || this.errorMessageTemplate;
+    }
   }
 };
 </script>
