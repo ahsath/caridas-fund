614b24d61dec914ac7976a1e35bc89f6d6c7c714
Ahsath <ahsath@gmail.com>
2020-04-21 18:25:41 -0500

    Update AppCreateRequest

--- src/components/AppCreateRequest.vue
+++ src/components/AppCreateRequest.vue
@@ -6,24 +6,41 @@
       </v-app-bar-nav-icon>
       <v-toolbar-title>Pedir ayuda</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn class="normal-case" @click="$refs.geolocationDialog.open = true" text>Publicar</v-btn>
+      <v-btn
+        class="normal-case"
+        @click="publish"
+        :disabled="!isGeoSupported || isGeoPermissionDenied || isPositionUnavailable"
+        text
+      >Publicar</v-btn>
     </v-app-bar>
 
     <v-card tile>
-      <v-banner class="mx-auto relative" v-model="banner" sticky app>
+      <v-banner class="mx-auto relative" v-model="showBanner" sticky app>
         <v-avatar slot="icon" color="primary" size="40">
           <v-icon class="grey--text text--darken-4" size="24">{{ mdiSatelliteVariant }}</v-icon>
         </v-avatar>Habilita la geolocalización del dispositivo para publicar tu solicitud en el mapa
         <template
           #actions
         >
-          <v-btn class="normal-case" @click.stop="banner = !banner" color="primary" text>Despúes</v-btn>
-          <v-btn class="normal-case" @click.stop color="primary" text>Habilitar</v-btn>
+          <v-btn class="normal-case" @click.stop="enableGeolocation" color="primary" text>Habilitar</v-btn>
         </template>
       </v-banner>
 
       <v-content>
         <v-container class="pa-4">
+          <v-row justify="center" dense>
+            <v-col cols="12" sm="10" md="6" lg="5" xl="4">
+              <v-alert
+                v-model="showAlert"
+                border="left"
+                :type="alert.type"
+                :color="alert.color"
+                dense
+                text
+                :icon="alert.icon"
+              >{{ alert.msg }}</v-alert>
+            </v-col>
+          </v-row>
           <v-row justify="center" dense>
             <v-col cols="12" sm="10" md="6" lg="5" xl="4">
               <v-text-field
@@ -66,8 +83,11 @@
                 required
                 persistent-hint
               ></v-text-field>
-              <v-icon class="text--disabled">{{ mdiCircleMedium }}</v-icon>
-              <span class="caption text--disabled">Ubicación desconocida</span>
+              <v-icon v-if="getCityAndCountry" class="text--secondary" x-small>{{ mdiMapMarker }}</v-icon>
+              <v-icon v-else class="text--disabled" x-small>{{ mdiCrosshairsOff }}</v-icon>
+              <span
+                class="caption text--disabled ml-2"
+              >{{ getCityAndCountry || 'Ubicación desconocida' }}</span>
             </v-col>
           </v-row>
           <v-row justify="center" class="mt-2" dense>
@@ -101,10 +121,10 @@
               </v-radio-group>
             </v-col>
           </v-row>
+          <v-snackbar v-model="showSnack" left>{{ snackMessage }}</v-snackbar>
         </v-container>
       </v-content>
     </v-card>
-    <app-enable-geolocation ref="enableGeolocation"></app-enable-geolocation>
   </v-dialog>
 </template>
 
@@ -114,12 +134,13 @@ import {
   mdiSatelliteVariant,
   mdiCellphoneAndroid,
   mdiMapMarker,
-  mdiCircleMedium,
-  mdiHelpCircle
+  mdiHelpCircle,
+  mdiMapMarkerOff,
+  mdiCrosshairsOff,
+  mdiCrosshairsQuestion
 } from "@mdi/js";
 
-import { createNamespacedHelpers } from "vuex";
-const { mapGetters } = createNamespacedHelpers("user");
+import { mapGetters, mapActions } from "vuex";
 
 export default {
   data: () => ({
@@ -146,22 +167,98 @@ export default {
       { text: "Los que puedan", color: "blue lighten-1", code: 4, emoji: "🙏🏼" }
     ],
     open: true,
-    banner: true,
+    alert: {
+      type: null,
+      msg: null,
+      color: null,
+      icon: null
+    },
+    showSnack: false,
+    snackMessage: null,
     mdiClose,
     mdiSatelliteVariant,
     mdiCellphoneAndroid,
     mdiMapMarker,
-    mdiCircleMedium,
-    mdiHelpCircle
+    mdiHelpCircle,
+    mdiMapMarkerOff,
+    mdiCrosshairsOff,
+    mdiCrosshairsQuestion
   }),
-  components: {
-    AppEnableGeolocation: () =>
-      import(
-        /* webpackChunkName: "AppEnableGeolocation" */ "./AppEnableGeolocation"
-      )
-  },
   computed: {
-    ...mapGetters(["getName"])
+    ...mapGetters([
+      "user/getName",
+      "getCityAndCountry",
+      "isGeoEnabled",
+      "getGeoPermission"
+    ]),
+    showAlert() {
+      if (!this.isGeoSupported) {
+        this.alert.msg =
+          "La geolocalización no esta soportada en este buscador";
+        this.alert.type = "error";
+        this.alert.color = "red lighten-3";
+        this.alert.icon = this.mdiMapMarkerOff;
+        return true;
+      }
+      if (this.isGeoPermissionDenied) {
+        this.alert.msg =
+          "Has bloqueado el acceso a tu ubicación. Para habilitarlo nuevamente, ve a la configuración de tu navegador ⚙️";
+        this.alert.type = "error";
+        this.alert.color = "red lighten-3";
+        this.alert.icon = this.mdiCrosshairsOff;
+        return true;
+      }
+      if (this.isPositionUnavailable) {
+        this.alert.msg =
+          "No pudimos obtener tu ubicación. Intenta nuevamente actualizando tu navegador o habilitando la ubicación en tu dispositivo";
+        this.alert.type = "error";
+        this.alert.color = "red lighten-3";
+        this.alert.icon = this.mdiCrosshairsQuestion;
+        return true;
+      }
+      return false;
+    },
+    isGeoSupported() {
+      return "geolocation" in navigator;
+    },
+    isGeoPermissionGranted() {
+      return this.getGeoPermission === "granted";
+    },
+    isGeoPermissionDenied() {
+      return this.getGeoPermission === "denied";
+    },
+    isPositionUnavailable() {
+      return this.getGeoPermission === "POSITION_UNAVAILABLE";
+    },
+    showBanner() {
+      return this.isGeoSupported === !this.isGeoEnabled;
+    }
+  },
+  watch: {
+    getGeoPermission: {
+      immediate: true,
+      handler: function(permission) {
+        switch (this.getGeoPermission) {
+          case "granted":
+            this.snackMessage = "La geolocalización está habilitada";
+            this.showSnack = true;
+            break;
+          case "denied":
+            this.snackMessage = "La geolocalización ha sido deshabilitada";
+            this.showSnack = true;
+            break;
+        }
+      }
+    }
+  },
+  methods: {
+    ...mapActions(["enableGeolocation"]),
+    publish() {
+      if (this.isGeoEnabled) {
+      }
+      this.snackMessage = "Habilita la geolocalización para publicar";
+      this.showSnack = true;
+    }
   },
   mounted() {
     this.person.name = this.getName;
