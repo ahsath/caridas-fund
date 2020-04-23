82885cf3e33b6a02a8bf9b2b847793078167e153
Ahsath <ahsath@gmail.com>
2020-04-22 19:02:27 -0500

    Add AppBrowserNotice.vue

---
+++ src/components/AppBrowserNotice.vue
@@ -0,0 +1,8 @@
+<template>
+  <v-dialog :value="true" max-width="320" persistent>
+    <v-card elevation="24">
+      <v-card-title>Brave no es compatible</v-card-title>
+      <v-card-text>Hemos detectado que estas usando un navegador que no es compatible con la aplicación, te recomendamos usar Google Chrome, Mozilla Firefox o Microsoft Edge</v-card-text>
+    </v-card>
+  </v-dialog>
+</template>
