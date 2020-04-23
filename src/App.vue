8d0a8f3dd27a5f79d520e9c3850ca9d5c9c6547f
Ahsath <ahsath@gmail.com>
2020-04-22 19:03:28 -0500

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,27 @@
 <template>
-  <router-view />
+  <v-app>
+    <router-view v-if="isBrowserSupported" />
+    <app-browser-notice v-else></app-browser-notice>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    isBrowserSupported: false
+  }),
+  components: {
+    AppBrowserNotice: () =>
+      import(
+        /* webpackChunkName: "AppBrowserNotice.vue" */ "./components/AppBrowserNotice"
+      )
+  },
+  created() {
+    if (typeof navigator.brave === "undefined") {
+      this.isBrowserSupported = true;
+    }
+  }
+};
+</script>
 
 
