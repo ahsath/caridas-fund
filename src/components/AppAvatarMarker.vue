2a538fc973605484b1b0d66bd33f0f0d091683a9
Ahsath <ahsath@gmail.com>
2020-04-26 01:24:55 -0500

    Add AppAvatarMarker.vue

---
+++ src/components/AppAvatarMarker.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="relative inline-block" data-badge="🚨">
+    <v-avatar class="border-2 border-solid border-dark rounded-full" size="36">
+      <img
+        src="https://lh3.googleusercontent.com/a-/AOh14Gh6uh6HAhFfkICQd6_1T2qPBXikvKveouW7HGw7_g"
+      />
+    </v-avatar>
+    <v-tooltip eager right open-on-click>
+      <template #activator="{ on }">
+        <span class="absolute -top-10px -right-5px title w-5 h-5" v-on="on">🚨</span>
+      </template>
+      Emergencia
+    </v-tooltip>
+  </div>
+</template>
