d46700edfca1f1065ca05dc123ea7328f25b9647
Ahsath <ahsath@gmail.com>
2020-04-26 01:41:58 -0500

    Update src/store/modules/user/mutations.js

--- src/store/modules/user/mutations.js
+++ src/store/modules/user/mutations.js
@@ -1,12 +1,32 @@
-export const onSuccessfulLogin = (state, { name, email, photoURL }) => {
-    state.user.name = name
-    state.user.email = email
-    state.user.photoURL = photoURL
-    state.user.isLoggedIn = true
+export const setUserData = (state, { uid, timestamp, name, photoURL, address, country, countryCode, casePriority, phoneNumber, request, email, coordinates = [] }) => {
+    state.uid = uid || ''
+    state.timestamp = timestamp || ''
+    state.name = name || ''
+    state.photoURL = photoURL || ''
+    state.address = address || ''
+    state.country = country || state.country
+    state.countryCode = countryCode || state.countryCode
+    state.casePriority = casePriority || ''
+    state.phoneNumber = phoneNumber || ''
+    state.request = request || ''
+    state.email = email || ''
+    state.coordinates.lat = coordinates[0] || state.coordinates.lat
+    state.coordinates.lng = coordinates[1] || state.coordinates.lng
 }
-export const onSignOut = state => {
-    state.user.name = null
-    state.user.email = null
-    state.user.photoURL = null
-    state.user.isLoggedIn = false
+export const clearUserInfo = state => {
+    state.uid = ''
+    state.timestamp = ''
+    state.name = ''
+    state.photoURL = ''
+    state.address = ''
+    state.casePriority = ''
+    state.phoneNumber = ''
+    state.request = ''
+    state.email = ''
 }
+export const updateGeoCoords = (state, { lat, lng }) => {
+    state.coordinates.lat = lat
+    state.coordinates.lng = lng
+}
+export const updateCountry = (state, country) => state.country = country
+export const updateCountryCode = (state, countryCode) => state.countryCode = countryCode
