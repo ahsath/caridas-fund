38b810fef6b59469579ed91e9e6fbd843090564e
Ahsath <ahsath@gmail.com>
2020-04-26 21:09:11 -0500

    Update src/store/modules/db/actions.js

--- src/store/modules/db/actions.js
+++ src/store/modules/db/actions.js
@@ -13,10 +13,36 @@ const requestsRef = geofirestore.collection('requests')
 export const getUserData = async ({ commit }, uid) => {
     const query = requestsRef.where('uid', "==", uid)
     const querySnapshot = await query.get()
-
     if (!querySnapshot.empty) {
-        const { uid, timestamp, name, photoURL, address, country, countryCode, casePriority, phoneNumber, request, coordinates } = querySnapshot.docs[0].data
-        commit('user/setUserData', { uid, timestamp, name, photoURL, address, country, countryCode, casePriority, phoneNumber, request, coordinates })
-        return
+        commit('setUserDocId', querySnapshot.docs[0].id)
+        const { name, timestamp, address, country, countryCode, casePriority, phoneNumber, request, coordinates } = querySnapshot.docs[0].data()
+        return Promise.resolve({ name, timestamp, address, country, countryCode, casePriority, phoneNumber, request, coordinates })
     }
+}
+
+export const saveUserRequest = ({ commit }, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const docRef = await requestsRef.add(data)
+            commit('setUserDocId', docRef.id)
+            resolve('Tu solicitud se ha guardado y será visible en breve')
+        } catch (e) {
+            console.log(e);
+            reject('Hubo un problema al guardar tu solicitud')
+        }
+    })
+}
+
+export const updateUserRequest = ({ commit, getters }, data) => {
+    return new Promise(async (resolve, reject) => {
+        const docRef = requestsRef.doc(getters.getUserDocId)
+        try {
+            await docRef.update(data)
+            commit('user/setUserData', data)
+            resolve('Tu solicitud se ha actualizado exitosamente')
+        } catch (e) {
+            console.log(e);
+            reject('Hubo un problema al actualizar tu solicitud')
+        }
+    })
 }
