5804f27792e3f49fbf149618b276c41c8c53ab51
Ahsath <ahsath@gmail.com>
2020-04-29 03:31:48 -0500

    Update src/store/modules/user/mutations.js

--- src/store/modules/user/mutations.js
+++ src/store/modules/user/mutations.js
@@ -1,26 +1,12 @@
-export const setUserData = (state, { uid, timestamp, name, photoURL, address, country, countryCode, casePriority, phoneNumber, request, email, coordinates = { latitude: '', longitude: '' } }) => {
-    state.uid = uid || state.uid
-    state.timestamp = timestamp || state.timestamp
-    state.name = name || state.name
-    state.photoURL = photoURL || state.photoURL
-    state.address = address || state.address
-    state.country = country || state.country
-    state.countryCode = countryCode || state.countryCode
-    state.casePriority = casePriority || 2
-    state.phoneNumber = phoneNumber || state.phoneNumber
-    state.request = request || state.request
-    state.email = email || state.email
-    state.coordinates.lat = coordinates.latitude || state.coordinates.lat
-    state.coordinates.lng = coordinates.longitude || state.coordinates.lng
+export const setUserData = (state, { uid, name, photoURL, email }) => {
+    state.uid = uid
+    state.name = name
+    state.photoURL = photoURL
+    state.email = email
 }
-
-export const updateGeoCoords = (state, { lat, lng }) => {
-    state.coordinates.lat = lat
-    state.coordinates.lng = lng
+export const updateGeoCoords = (state, coords) => {
+    state.coordinates.lat = coords.latitude
+    state.coordinates.lng = coords.longitude
 }
-
 export const updateCountry = (state, country) => state.country = country
-
 export const updateCountryCode = (state, countryCode) => state.countryCode = countryCode
-
-export const updateTimestamp = (state, timestamp) => state.timestamp = timestamp 
