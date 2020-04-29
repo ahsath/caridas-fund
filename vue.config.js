7bbb91aaf0db3effa64cb2efc81b1d33a0b1605f
Ahsath <ahsath@gmail.com>
2020-04-29 03:23:17 -0500

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 module.exports = {
   "transpileDependencies": [
     "vuetify"
-  ]
+  ],
+  pwa: {
+    name: 'Caridas',
+    themeColor: '#90caf9',
+    msTileColro: '#121212',
+    appleMobileWebAppCapable: 'yes',
+    appleMobileWebAppStatusBarStyle: '#121212',
+    workboxOptions: {
+      skipWaiting: true
+    }
+  }
 }
