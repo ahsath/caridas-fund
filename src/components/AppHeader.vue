983eca6673270e952a52079e76667041fd7d04c9
Ahsath <ahsath@gmail.com>
2020-04-29 18:14:51 -0500

    Add AppHeader.vue

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app>
+    <v-toolbar-title>
+      <router-link to="/" class="d-flex align-center no-underline">
+        <v-icon color="primary" x-large>$vuetify.icons.values.logo</v-icon>
+        <span class="ml-2 font-weight-bold white--text">Caridas</span>
+      </router-link>
+    </v-toolbar-title>
+    <v-btn to="/acerca" tag="a" class="text--secondary normal-case ml-4" text>
+      <v-icon left>{{ mdiInformation }}</v-icon>Acerca
+    </v-btn>
+    <v-spacer></v-spacer>
+    <slot name="section:right"></slot>
+  </v-app-bar>
+</template>
+
+<script>
+import { mdiInformation } from "@mdi/js";
+export default {
+  data: () => ({
+    mdiInformation
+  })
+};
+</script>
