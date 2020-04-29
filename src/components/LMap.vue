58f7b02db6f80b98ad89617a2c53f48a4778bf49
Ahsath <ahsath@gmail.com>
2020-04-29 03:27:05 -0500

    Update LMap.vue

--- src/components/LMap.vue
+++ src/components/LMap.vue
@@ -3,41 +3,30 @@
 </template>
 
 <script>
-import { mapGetters, mapActions } from "vuex";
+import { mapGetters, mapActions, mapMutations } from "vuex";
 import "leaflet/dist/leaflet.css";
 import L, { Marker, DivIcon } from "leaflet";
 import AppAvatarMarker from "./AppAvatarMarker.vue";
 
 export default {
   data: () => ({
-    map: undefined,
-    lat: 0,
-    lng: 0,
-    avatarMarker: undefined,
+    l: {
+      map: undefined,
+      lat: 0,
+      lng: 0
+    },
+    marker: null,
+    person: null,
     accessToken:
       "pk.eyJ1IjoiYWhzYXRoIiwiYSI6ImNqeTI5OXQ2MzBtY3ozam14bWg3NDZmdmIifQ.DbVblAkBv6_QrIypX0vA8Q"
   }),
-  components: {},
   watch: {
-    getCoords: {
-      handler() {
-        // this.map.flyTo([this.getCoords.lat, this.getCoords.lng], 16);
-        // new Marker([this.getCoords.lat, this.getCoords.lng], {
-        //   icon: new DivIcon({
-        //     html: this.avatarMarker,
-        //     iconSize: [36, 36],
-        //     iconAnchor: [18, 36],
-        //     className: ""
-        //   }),
-        //   riseOnHover: true
-        // })
-        //   .addTo(this.map)
-        //   .on("click", e => {
-        //     // console.log(e);
-        //   });
-      },
-      deep: true
-    }
+    // getCoords: {
+    //   handler() {
+    //     this.l.map.flyTo([this.getCoords.lat, this.getCoords.lng], 16);
+    //   },
+    //   deep: true
+    // },
   },
   computed: {
     ...mapGetters({
@@ -46,12 +35,18 @@ export default {
     })
   },
   methods: {
-    ...mapActions("user", ["setCountry", "setCountryCode"]),
+    ...mapActions(["getRequests"]),
+    ...mapMutations("user", ["updateCountry", "updateCountryCode"]),
     init() {
-      this.map = L.map("map", {
+      this.l.map = L.map("map", {
         attributionControl: false,
-        zoomControl: false
-      }).setView([this.lat, this.lng], 6);
+        zoomControl: false,
+        minZoom: 3,
+        maxBounds: [
+          [84.88148837022706, 191.07421875],
+          [-84.92832092949963, -169.27734375000003]
+        ]
+      }).setView([this.l.lat, this.l.lng], 6);
 
       // to do: add apiKey to .prod.env
       L.tileLayer(
@@ -64,7 +59,44 @@ export default {
           zoomOffset: -1,
           accessToken: this.accessToken
         }
-      ).addTo(this.map);
+      ).addTo(this.l.map);
+
+      this.addMapMarkers();
+    },
+    async addMapMarkers() {
+      const query = await this.getRequests();
+      const AvatarMaker = this.$vue.extend(AppAvatarMarker);
+      let person, avatarMarker;
+      if (!query.empty) {
+        query.docs.forEach(doc => {
+          person = doc.data();
+          avatarMarker = new AvatarMaker({
+            propsData: { person }
+          }).$mount();
+          this.createHTMLMarker(
+            avatarMarker.$el,
+            doc.data().coordinates.latitude,
+            doc.data().coordinates.longitude
+          );
+        });
+      }
+    },
+    createHTMLMarker(el, lat, lng) {
+      let person, marker;
+      marker = new Marker([lat, lng], {
+        icon: new DivIcon({
+          html: el,
+          iconSize: [36, 36],
+          iconAnchor: [18, 36],
+          className: ""
+        }),
+        riseOnHover: true
+      })
+        .addTo(this.l.map)
+        .on("click", () => {
+          person = marker.getIcon().options.html.dataset;
+          this.$emit("click:marker", person);
+        });
     }
   },
   async created() {
@@ -78,26 +110,20 @@ export default {
           const data = await res.json();
           const { country_name, country_code2, latitude, longitude } = data;
           if (!!this.getUserCoords.lat === false) {
-            this.lat = latitude;
-            this.lng = longitude;
+            this.l.lat = latitude;
+            this.l.lng = longitude;
           } else {
-            this.lat = this.getUserCoords.lat;
-            this.lng = this.getUserCoords.lng;
+            this.l.lat = this.getUserCoords.lat;
+            this.l.lng = this.getUserCoords.lng;
           }
           this.init();
-          this.setCountry(country_name);
-          this.setCountryCode(country_code2);
+          this.updateCountry(country_name);
+          this.updateCountryCode(country_code2);
         }
       } catch (e) {
         console.log(e);
       }
     }
-  },
-  mounted() {
-    // const AvatarMarker = this.$vue.extend(AppAvatarMarker);
-    // const avatarMarker = new AvatarMarker().$mount();
-    // console.log(avatarMarker.$el);
-    // this.avatarMarker = avatarMarker.$el;
   }
 };
 </script>
