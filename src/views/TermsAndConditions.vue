8535c571550a16791e05d2908061698443d651b1
Ahsath <ahsath@gmail.com>
2020-04-29 18:14:15 -0500

    Update TermsAndConditions.vue

--- src/views/TermsAndConditions.vue
+++ src/views/TermsAndConditions.vue
@@ -1,3 +1,55 @@
 <template>
-  <h1>Terminos y condiciones</h1>
+  <section>
+    <app-header></app-header>
+    <v-content>
+      <v-container>
+        <v-card max-width="724" class="mx-auto">
+          <v-card-title>Términos y Condiciones</v-card-title>
+
+          <v-card-text>Estos términos y condiciones ("Términos", "Acuerdo") son un acuerdo entre el Operador del sitio web ("Operador del sitio web", "nosotros", "nosotros" o "nuestro") y usted ("Usuario", "usted" o "su "). Este Acuerdo establece los términos y condiciones generales de su uso del sitio web caridas.fund y cualquiera de sus productos o servicios (colectivamente, "Sitio web" o "Servicios").</v-card-text>
+
+          <v-card-title class="subtitle-1">Cuentas y membresía</v-card-title>
+          <v-card-text>Si crea una cuenta en el sitio web, usted es responsable de mantener la seguridad de su cuenta y es totalmente responsable de todas las actividades que ocurran bajo la cuenta y cualquier otra acción tomada en relación con ella. Podemos, pero no tenemos la obligación de, monitorear y revisar nuevas cuentas antes de que pueda iniciar sesión y usar nuestros Servicios. Proporcionar información de contacto falsa de cualquier tipo puede resultar en la cancelación de su cuenta. Debe notificarnos de inmediato sobre cualquier uso no autorizado de su cuenta o cualquier otra violación de seguridad. No seremos responsables de ningún acto u omisión por su parte, incluidos los daños de cualquier tipo incurridos como resultado de dichos actos u omisiones. Podemos suspender, deshabilitar o eliminar su cuenta (o cualquier parte de la misma) si determinamos que ha violado alguna disposición de este Acuerdo o que su conducta o contenido tenderá a dañar nuestra reputación y buena voluntad. Si eliminamos su cuenta por los motivos anteriores, no puede volver a registrarse en nuestros Servicios. Podemos bloquear su dirección de correo electrónico y dirección de protocolo de Internet para evitar un mayor registro.</v-card-text>
+
+          <v-card-title class="subtitle-1">Contenido del usuario</v-card-title>
+          <v-card-text>No poseemos ningún dato, información o material ("Contenido") que envíe en el Sitio web en el transcurso del uso del Servicio. Usted será el único responsable de la precisión, calidad, integridad, legalidad, fiabilidad, idoneidad y propiedad intelectual o derecho de uso de todo el Contenido enviado. Podemos, pero no tenemos la obligación de, monitorear y revisar el Contenido en el Sitio web enviado o creado utilizando nuestros Servicios por usted. A menos que usted lo permita específicamente, su uso del sitio web no nos otorga la licencia para usar, reproducir, adaptar, modificar, publicar o distribuir el contenido creado por usted o almacenado en su cuenta de usuario con fines comerciales, de marketing o similares. Pero nos otorga permiso para acceder, copiar, distribuir, almacenar, transmitir, reformatear, mostrar y ejecutar el Contenido de su cuenta de usuario únicamente según sea necesario con el fin de proporcionarle los Servicios. Sin limitar ninguna de esas representaciones o garantías, tenemos el derecho, aunque no la obligación, de, a nuestro exclusivo criterio, rechazar o eliminar cualquier Contenido que, en nuestra opinión razonable, viole cualquiera de nuestras políticas o sea de alguna manera perjudicial o objetable.</v-card-text>
+
+          <v-card-title class="subtitle-1">Copias de seguridad</v-card-title>
+          <v-card-text>No somos responsables del contenido que reside en el sitio web. En ningún caso seremos responsables de la pérdida de ningún Contenido. Es su exclusiva responsabilidad mantener una copia de seguridad adecuada de su Contenido. Sin perjuicio de lo anterior, en algunas ocasiones y en determinadas circunstancias, sin ninguna obligación, podremos restaurar algunos o todos sus datos que se hayan eliminado a partir de una fecha y hora determinadas en las que hayamos hecho una copia de seguridad de nuestros propios datos. propósitos No garantizamos que los datos que necesita estarán disponibles.</v-card-text>
+
+          <v-card-title class="subtitle-1">Enlaces a otros sitios web</v-card-title>
+          <v-card-text>Aunque este sitio web puede vincular a otros sitios web, no estamos, directa o indirectamente, implicando ninguna aprobación, asociación, patrocinio, respaldo o afiliación con ningún sitio web vinculado, a menos que se indique específicamente en este documento. No somos responsables de examinar o evaluar, y no garantizamos las ofertas de ninguna empresa o individuo ni el contenido de sus sitios web. No asumimos ninguna responsabilidad u obligación por las acciones, productos, servicios y contenido de otros terceros. Debe revisar cuidadosamente las declaraciones legales y otras condiciones de uso de cualquier sitio web al que acceda a través de un enlace desde este sitio web. Su enlace a cualquier otro sitio web fuera del sitio es bajo su propio riesgo.</v-card-text>
+
+          <v-card-title class="subtitle-1">Usos prohibidos</v-card-title>
+          <v-card-text>Además de otros términos establecidos en el Acuerdo, se le prohíbe usar el Sitio web o su Contenido: (a) para cualquier propósito ilegal; (b) solicitar a otros que realicen o participen en actos ilegales; (c) violar cualquier regulación, norma, ley u ordenanza local internacional, federal, provincial o estatal; (d) infringir o violar nuestros derechos de propiedad intelectual o los derechos de propiedad intelectual de terceros; (e) acosar, abusar, insultar, dañar, difamar, calumniar, menospreciar, intimidar o discriminar por motivos de género, orientación sexual, religión, origen étnico, raza, edad, origen nacional o discapacidad; (f) presentar información falsa o engañosa; (g) para cargar o transmitir virus o cualquier otro tipo de código malicioso que se utilizará o podrá utilizarse de cualquier manera que afecte la funcionalidad u operación del Servicio o de cualquier sitio web relacionado, otros sitios web o Internet; (h) para recopilar o rastrear la información personal de otros; (i) a spam, phishing, pharm, pretexto, araña, rastreo o raspado; (j) para cualquier propósito obsceno o inmoral; o (k) interferir o eludir las características de seguridad del Servicio o cualquier sitio web relacionado, otros sitios web o Internet. Nos reservamos el derecho de terminar su uso del Servicio o cualquier sitio web relacionado por violar cualquiera de los usos prohibidos.</v-card-text>
+
+          <v-card-title class="subtitle-1">Derechos de propiedad intelectual</v-card-title>
+          <v-card-text>Este Acuerdo no le transfiere ninguna propiedad intelectual que sea propiedad del Operador del sitio web o de terceros, y todos los derechos, títulos e intereses sobre dicha propiedad permanecerán (entre las partes) únicamente con el Operador del sitio web. Todas las marcas comerciales, marcas de servicio, gráficos y logotipos utilizados en relación con nuestro sitio web o servicios, son marcas comerciales o marcas comerciales registradas del operador del sitio web o los licenciantes del operador del sitio web. Otras marcas comerciales, marcas de servicio, gráficos y logotipos utilizados en relación con nuestro sitio web o servicios pueden ser marcas comerciales de otros terceros. El uso de nuestro sitio web y nuestros servicios no le otorga ningún derecho o licencia para reproducir o utilizar de cualquier otro modo ningún operador del sitio web o marcas comerciales de terceros.</v-card-text>
+
+          <v-card-title class="subtitle-1">Indemnización</v-card-title>
+          <v-card-text>Usted acepta indemnizar y eximir de responsabilidad al Operador del sitio web y sus afiliados, directores, funcionarios, empleados y agentes de cualquier responsabilidad, pérdida, daño o costo, incluidos los honorarios razonables de abogados, incurridos en conexión con cualquier tercero. acusaciones, reclamos, acciones, disputas o demandas presentadas contra cualquiera de ellos como resultado de o relacionado con su Contenido, su uso del Sitio web o los Servicios o cualquier mala conducta intencional de su parte.</v-card-text>
+
+          <v-card-title class="subtitle-1">Cambios y enmiendas</v-card-title>
+          <v-card-text>Nos reservamos el derecho de modificar este Acuerdo o sus políticas relacionadas con el Sitio web o los Servicios en cualquier momento, a partir de la publicación de una versión actualizada de este Acuerdo en el Sitio web. Cuando lo hagamos, publicaremos una notificación en la página principal de nuestro sitio web. El uso continuo del sitio web después de dichos cambios constituirá su consentimiento para dichos cambios. La política se creó con WebsitePolicies.</v-card-text>
+
+          <v-card-title class="subtitle-1">Aceptación de estos términos.</v-card-title>
+          <v-card-text>Usted reconoce que ha leído este Acuerdo y acepta todos sus términos y condiciones. Al utilizar el sitio web o sus servicios, usted acepta estar sujeto a este Acuerdo. Si no acepta cumplir con los términos de este Acuerdo, no está autorizado para usar o acceder al Sitio web y sus Servicios.</v-card-text>
+
+          <v-card-title class="subtitle-1">Contactando con nosotros</v-card-title>
+          <v-card-text>Si desea comunicarse con nosotros para conocer más acerca de este Acuerdo o desea comunicarse con nosotros sobre cualquier asunto relacionado con él, puede enviar un correo electrónico a ahsath@gmail.com</v-card-text>
+
+          <v-card-text>Este documento fue actualizado por última vez el 29 de abril de 2020</v-card-text>
+        </v-card>
+      </v-container>
+    </v-content>
+  </section>
 </template>
+
+<script>
+import AppHeader from "../components/AppHeader";
+export default {
+  components: {
+    AppHeader
+  }
+};
+</script>
