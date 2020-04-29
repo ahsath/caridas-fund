d117176660cc06c6ed597400a4a2daf0c73914c2
Ahsath <ahsath@gmail.com>
2020-04-29 03:30:57 -0500

    Update src/store/modules/user/getters.js

--- src/store/modules/user/getters.js
+++ src/store/modules/user/getters.js
@@ -1,12 +1,7 @@
 // To do: Add <getUserInfo> Simplify it by adding a filter option
 export const getUserPhotoURL = ({ photoURL }) => photoURL
 export const getUserName = ({ name }) => name
-export const getUserPhoneNumber = ({ phoneNumber }) => phoneNumber
-export const getUserAddress = ({ address }) => address
-export const getUserRequest = ({ request }) => request
 export const getCountryCode = ({ countryCode }) => countryCode
 export const getCountry = ({ country }) => country
 export const getCoordinates = ({ coordinates }) => coordinates
-export const getUserCasePriorityCode = ({ casePriority }) => casePriority
 export const getUserUID = ({ uid }) => uid
-export const getTimestamp = ({ timestamp }) => timestamp
