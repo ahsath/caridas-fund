6bbc95bcc76ad4b1706533ae20eec553d5e187a1
Ahsath <ahsath@gmail.com>
2020-04-29 19:42:32 -0500

    Update IsLoading.vue

--- src/components/IsLoading.vue
+++ src/components/IsLoading.vue
@@ -1,3 +1,5 @@
 <template>
-  <div style="position: fixed; left: 50%; top: 50%; font-size: 48px; color: white">Cargando...</div>
+  <div class="fixed top-0 left-0 h-min-height w-screen d-flex justify-center align-center">
+    <v-progress-circular color="primary" indeterminate></v-progress-circular>
+  </div>
 </template>
