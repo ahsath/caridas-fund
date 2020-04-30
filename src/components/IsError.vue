77620189563818355517351ef41a41884a86c41a
Ahsath <ahsath@gmail.com>
2020-04-30 16:57:00 -0500

    Update error message

--- src/components/IsError.vue
+++ src/components/IsError.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="fixed top-0 left-0 h-min-height w-screen d-flex justify-center align-center">
-    <h1>Hubo un error al cargar la aplicación</h1>
+    <div>
+      <h1 class="title">Hubo un error al cargar el mapa</h1>
+      <p class="caption text--secondary text-center mt-3">Actualiza la pagina nuevamente</p>
+    </div>
   </div>
 </template>
